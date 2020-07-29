64e851f894e175fb06b3fde975e3a55633a2ce2c
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-07-29 06:24:33 +0100

    Returned logging level to info

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -22,8 +22,8 @@ def getLogger(name):
 
     # create console handler and set level to debug
     ch = logging.StreamHandler()
-    # ch.setLevel(logging.INFO)
-    ch.setLevel(logging.DEBUG)
+    ch.setLevel(logging.INFO)
+    # ch.setLevel(logging.DEBUG)
 
     # create logger
     logger = logging.getLogger(name)
