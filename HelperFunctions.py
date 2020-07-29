443599d63264ab4513c4476ccdea542c1c414c80
Lukasz Baldyga <c3ypt1c@gmail.com>
2020-07-29 07:19:45 +0100

    Fixed logger bug where everything would double

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1,3 +1,6 @@
+import logging
+
+
 def Save(file, content):
     f = open(file, "w")
     f.write(content)
@@ -11,25 +14,24 @@ def Read(file):
     return fd
 
 
+# create formatter
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s - [%(filename)s:%(lineno)d] - %(message)s')
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# ch.setLevel(logging.DEBUG)
+
+# create logger
+logger = logging.getLogger("")
+logger.setLevel(logging.DEBUG)
+
+# add formatter to ch and add ch to logger
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logger.debug("Logger created")
+
+
 def getLogger():
-    import logging
-
-    # create formatter
-    formatter = logging.Formatter('%(asctime)s - [%(filename)s:%(lineno)d] - %(message)s')
-
-    # create console handler and set level to debug
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    # ch.setLevel(logging.DEBUG)
-
-    # create logger
-    logger = logging.getLogger("")
-    logger.setLevel(logging.DEBUG)
-
-    # add formatter to ch and add ch to logger
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    logger.debug("Logger created")
-
     return logger
